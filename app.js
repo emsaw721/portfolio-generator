075d55e87ad3556b6b9348800f435573c31e9bf6
Emily Hill <emhillmed2021@gmail.com>
2022-10-03 21:43:54 -0500

    added js files

---
+++ app.js
@@ -0,0 +1,16 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length); 
+console.log(profileDataArgs) 
+//Notice the lack of parentheses around the "profileDataArr" parameter. 
+const printProfileData = profileDataArr => {
+    // this... 
+    for (let i=0; i < profileDataArr.length; i+= 1){
+        console.log(profileDataArr[i]);
+    }
+
+    console.log("==============")
+
+    //is the same thing as this ...
+    profileDataArr.forEach(profileItem => console.log(profileItem)); 
+}; 
+
+printProfileData(profileDataArgs); 
